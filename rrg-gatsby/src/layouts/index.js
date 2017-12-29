fb56ca78e2af8e8905f73da3eff8a2dab011023a
Yan-Yin Choy <yanyinchoy@gmail.com>
2017-12-28 17:22:20 -0800

    replace titles with site metadata and add queries for site metadata -title

--- rrg-gatsby/src/layouts/index.js
+++ rrg-gatsby/src/layouts/index.js
@@ -10,17 +10,28 @@ const ListLink = props =>
     </Link>
   </li>
 
-export default ({ children }) =>
+export default ({ children, data }) =>
   <div style={{ margin: `0 auto`, maxWidth: 750, padding: `1.25rem 1rem` }}>
     <header style={{ marginBottom: `1.5rem` }}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>Renter's Rights Guide </h3>
+        <h3 style={{ display: `inline` }}>
+          {data.site.siteMetadata.title}
+        </h3>
       </Link>
       <ul style={{ listStyle: `none`, float: `right` }}>
-        <ListLink to="/">Home</ListLink>
         <ListLink to="/about/">About</ListLink>
         <ListLink to="/report/">Report an Issue</ListLink>
       </ul>
     </header>
     {children()}
   </div>
+
+export const query = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
--- rrg-gatsby/src/pages/about.js
+++ rrg-gatsby/src/pages/about.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-export default () => (
+export default ({data}) => (
   <div>
-    <h1>About me</h1>
+    <h1>About {data.site.siteMetadata.title}</h1>
     <p>Hot sauce lemonade avocado toast local organic siracha kimchi tacos </p>
   </div>
 );
+
+export const query = graphql`
+  query AboutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
