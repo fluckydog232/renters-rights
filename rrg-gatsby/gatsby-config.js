02ae166867f0a084434f8b68f6c615db9246bec0
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-01-12 18:09:07 -0800

    add plugins

--- rrg-gatsby/gatsby-config.js
+++ rrg-gatsby/gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: `San Jose Renter's Rights Guide`,
+    author: `Yan-Yin Choy <yanyinchoy@gmail.com>`,
+    twitter: `codeforsanjose`,
+    description: `Helping renters in San Jose, California understand the laws and programs in place to protect them.`
   },
   plugins: [
     `gatsby-transformer-remark`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-twitter`, 
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-remove-trailing-slashes`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,9 +21,50 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              // Remove the default behavior of adding a link to each
+              // image.
+              linkImagesToOriginal: true,
+              // Analyze images' pixel density to make decisions about
+              // target image size. This is what GitHub is doing when
+              // embedding images in tickets. This is a useful setting
+              // for documentation pages with a lot of screenshots.
+              // It can have unintended side effects on high pixel
+              // density artworks.
+              //
+              // Example: A screenshot made on a retina screen with a
+              // resolution of 144 (e.g. Macbook) and a width of 100px,
+              // will be rendered at 50px.
+              //
+              // Defaults to false.
+              sizeByPixelDensity: false,
+            },
+          },
+          `gatsby-remark-responsive-iframe`
+        ],
+      },
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
+      },
+    },  
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          trackingId: "UA-52748721-2",
+          // Setting this parameter is optional
+        anonymize: true,
       },
     },
   ],
