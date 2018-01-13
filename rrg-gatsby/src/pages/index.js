8cfecbb90c6550f024b28f54da42487e3456eb59
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-01-12 18:10:33 -0800

    update landing page

--- rrg-gatsby/src/pages/index.js
+++ rrg-gatsby/src/pages/index.js
@@ -8,4 +8,46 @@ const IndexPage = () => (
   </div>
 )
 
-export default IndexPage
+export default ({data }) => {
+  console.log(data);
+  return (
+    <div>
+      <h2 style={{display:"inline-block"}}>
+      Helping renters in San Jose, California understand the laws and programs in place to protect them.
+      </h2>
+      <br/>
+      <h2 style={{display:"inline-block"}}>
+      I need help with
+      </h2>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div className="container">
+          <div key={node.id}>
+            <h3 style={{marginBotton: `1.5rem`}}>
+              {node.frontmatter.title}{" "}
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        </div>
+      ))}
+
+    </div>
+  );
+};
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
