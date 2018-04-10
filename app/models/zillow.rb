acb8b1a3529801d0d119d0e9d0ac8fecae15cb3d
Yan-Yin Choy <yanyinchoy@gmail.com>
2018-04-10 09:11:05 -0700

    comment for alternative way

--- app/models/zillow.rb
+++ app/models/zillow.rb
@@ -1,9 +1,13 @@
+require 'HTTParty'
+
 class Zillow
   include HTTParty
   format :xml
   base_uri 'http://www.zillow.com/webservice'
 
   attr_accessor :zws_id, :http_timeout, :yearBuilt, :useCode, :city, :type
+
+  # https://www.zillow.com/howto/api/GetDeepSearchResults.htm has the full specifications of the API used
 
   def initialize(zws_id, http_timeout = nil)
     self.zws_id = zws_id
@@ -15,14 +19,24 @@ class Zillow
     self.type = type
   end
 
-  # Find yearBuilt
-  # Determine if built before September 7, 1979
-  # Find useCode
-  # Determine total units - if 2, 3+ or 4+
-  # Find city
-  # Determine if San Jose
-  # Find type
-  # Determine if region type=city || type =neighborhood
+    # Find yearBuilt. Determine if built before September 7, 1979
+    # Find useCode. Specifies type of property. Use useCode to determine total units - if 2, 3+ or 4+.
+    # Find city. Determine if it is "SAN JOSE"
+    # Find type. Determine if the type is neighborhood within San José; if it returns a city and the city="SAN JOSE",
+    # then it is actually in unincorporated County region
+    # def getInfo(key,address,citystatezip)
+    #   url="http://www.zillow.com/webservice/GetDeepSearchResults.htm?zws-id=#{key}&address=#{address}&citystatezip=#{citystatezip}"
+    #   page=HTTParty.get(url)
+    #   if (page.parsed_response["searchresults"]["message"]["code"] == "0")
+    #     useCode=page.parsed_response["searchresults"]["response"]["results"]["result"]["useCode"]
+    #     yearBuilt=page.parsed_response["searchresults"]["response"]["results"]["result"]["yearBuilt"]
+    #     bedrooms=page.parsed_response["searchresults"]["response"]["results"]["result"]["bedrooms"]
+    #     return [useCode,yearBuilt,bedrooms]
+    #   else
+    #     # Handle error here
+    #   end
+    # end
+    # getInfo(key,address,citystatezip);
 
   def query(address, citystatezip)
     options = { query: { address: address, citystatezip: citystatezip } }
@@ -34,9 +48,6 @@ class Zillow
         response["searchresults"]["response"]["results"]["result"]["yearBuit"],
         response["searchresults"]["response"]["results"]["result"]["localRealEstate"]["region type"]
       )
-    else
-      # raises the net/http response that was raised
-      raise response.response
     end
   end
 
